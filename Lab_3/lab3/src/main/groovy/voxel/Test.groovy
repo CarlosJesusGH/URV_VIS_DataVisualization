88e802ff7973beb3038e340085e81ed1d23a61eb
CarlosJesusGH <s.carlosj.28@gmail.com>
2017-05-03 10:57:51 +0200

    Lab 3: completed with some rendering errors

--- Lab_3/lab3/src/main/groovy/voxel/Test.groovy
+++ Lab_3/lab3/src/main/groovy/voxel/Test.groovy
@@ -16,16 +16,22 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		//Model model = fileTest("Engine.raw", 0.0f, 1.0f, [256,256,256], false, false)
+		// GOOD
+		Model model = fileTest("Engine.raw", 0.0f, 1.0f, [256,256,256], false, false)
 
+		// TODO: BAD, I don't know why, it should be the same than others 8 bits.
 		//Model model = fileTest("Tomato.raw", 0.0f, 1.0f, [256,256,64], false, false)
 
-		Model model = fileTest("present.dat", 0.0f, 1.0f, [246,246,221], true, true)
+		// TODO: BAD, maybe I'm reading 16 bits in a wrong way.
+		//Model model = fileTest("present.dat", 0.0f, 1.0f, [246,246,221], true, true)
 
+		// GOOD
 		//Model model = fileTest("nucleon.raw", 0.0f, 1.0f, [41,41,41], false, false)
 
+		// SEEMS GOOD, but I'm not sure.
 		//Model model = fileTest("marschnerlobb.raw", 0.0f, 1.0f, [41,41,41], false, false)
 
+		// GOOD
 		//Model model = fileTest("fuel.raw", 0.0f, 1.0f, [64,64,64], false, false)
 
 
